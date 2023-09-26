58fddf23a29630b76e6f4bd3f3cc324c786862b6
uoubyy <uoubyy@gmail.com>
2023-09-25 21:54:59 -0700

    create bp game mode base

--- Source/BombGame/BGGameMode.cpp
+++ Source/BombGame/BGGameMode.cpp
@@ -9,18 +9,4 @@ ABGGameMode::ABGGameMode()
 {
 	// use our custom PlayerController class
 	PlayerControllerClass = ABGPlayerController::StaticClass();
-
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
-	if (PlayerPawnBPClass.Class != nullptr)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
-
-	// set default controller to our Blueprinted controller
-	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownPlayerController"));
-	if(PlayerControllerBPClass.Class != NULL)
-	{
-		PlayerControllerClass = PlayerControllerBPClass.Class;
-	}
 }
