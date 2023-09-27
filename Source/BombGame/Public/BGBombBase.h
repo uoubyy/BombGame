dce6ba4bc1bf78297fc6e92c7083456f2932b103
uoubyy <uoubyy@gmail.com>
2023-09-27 12:33:32 -0700

    modify character

--- Source/BombGame/Public/BGBombBase.h
+++ Source/BombGame/Public/BGBombBase.h
@@ -68,6 +68,9 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "Bomb Game|Bomb")
 	float CurrentMovingSpeed;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "Bomb Game|Bomb")
+	int32 BombUniqueId;
+
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Bomb Game|Bomb")
 	void OnTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
@@ -75,9 +78,6 @@ protected:
 	void OnBombExploded();
 
 private:
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "Bomb Game|Bomb")
-	int32 BombUniqueId;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "Bomb Game|Bomb")
 	int32 DamageAmount;
